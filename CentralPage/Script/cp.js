b3a97d29693ed708e54a9ca4c8242fb22583bb92
Mohammed-glr <102376@glr.nl>
2024-10-10 14:56:10 +0000

    done

--- CentralPage/Script/cp.js
+++ CentralPage/Script/cp.js
@@ -13,6 +13,8 @@ const image1 = document.getElementById('hl');
 const image2 = document.getElementById('cm');
 const image3 = document.getElementById('gt');
 
-addGradient(image1, 'linear-gradient(  270deg,hsl(0deg 0% 100%) 0%,hsl(30deg 71% 89%) 20%,hsl(31deg 66% 78%) 40%,hsl(33deg 62% 66%) 60%,hsl(34deg 58% 54%) 80%,hsl(38deg 89% 39%) 100%)');
-addGradient(image2, 'linear-gradient( 180deg,hsl(0deg 0% 100%) 0%,hsl(340deg 100% 93%) 20%,hsl(339deg 100% 85%) 40%,hsl(338deg 96% 77%) 60%,hsl(335deg 89% 67%) 80%,hsl(329deg 85% 52%) 100%)');
-addGradient(image3, 'linear-gradient(  90deg,hsl(0deg 0% 100%) 0%,hsl(255deg 22% 83%) 20%,hsl(254deg 22% 67%) 40%,hsl(251deg 21% 51%) 60%,hsl(247deg 36% 36%) 80%,hsl(228deg 98% 18%) 100%)');
+addGradient(image1, 'linear-gradient(135deg,hsl(38deg 89% 39%) 0%,hsl(37deg 67% 51%) 20%,hsl(37deg 86% 61%) 40%,hsl(37deg 100% 74%) 60%,hsl(37deg 100% 87%) 80%,hsl(0deg 0% 100%) 100%)');
+addGradient(image2, 'linear-gradient(0deg,hsl(329deg 85% 52%) 0%,hsl(332deg 89% 70%) 20%,hsl(330deg 95% 83%) 40%,hsl(329deg 100% 90%) 60%,hsl(329deg 100% 95%) 80%,hsl(0deg 0% 100%) 100%)');
+addGradient(image3, 'linear-gradient(225deg,hsl(228deg 98% 18%) 0%,hsl(230deg 74% 31%) 20%,hsl(229deg 67% 43%) 40%,hsl(238deg 59% 63%) 60%,hsl(247deg 61% 81%) 80%,hsl(0deg 0% 100%) 100%)');
+
+// linear-gradient(135deg,hsl(38deg 89% 39%) 0%,hsl(37deg 67% 51%) 20%,hsl(37deg 86% 61%) 40%,hsl(37deg 100% 74%) 60%,hsl(37deg 100% 87%) 80%,hsl(0deg 0% 100%) 100%)
