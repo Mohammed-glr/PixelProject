f9210adacc3c2a594c4a4ef7f69d716a160c044d
Mohammed-glr <102376@glr.nl>
2024-10-12 21:47:21 +0000

    d

--- CentralPage/Script/cp.js
+++ CentralPage/Script/cp.js
@@ -1,4 +1,4 @@
-function addAnimatedGradientOnHover(element) {
+function addAnimatedGradientOnHover(elembent) {
     element.addEventListener('mouseover', () => {
         document.body.classList.add('animated-gradient');
     });
@@ -15,6 +15,3 @@ const container3 = document.getElementById('container3');
 addAnimatedGradientOnHover(container1);
 addAnimatedGradientOnHover(container2);
 addAnimatedGradientOnHover(container3);
-console.log("cp.js loaded")
-
-// linear-gradient(135deg,hsl(38deg 89% 39%) 0%,hsl(37deg 67% 51%) 20%,hsl(37deg 86% 61%) 40%,hsl(37deg 100% 74%) 60%,hsl(37deg 100% 87%) 80%,hsl(0deg 0% 100%) 100%)
