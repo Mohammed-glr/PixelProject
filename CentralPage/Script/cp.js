3a69205bf659d50e7569e6fb072417f1285529c5
Mohammed-glr <102376@glr.nl>
2024-10-15 11:37:05 +0000

    Done

--- CentralPage/Script/cp.js
+++ CentralPage/Script/cp.js
@@ -1,17 +1,17 @@
-function addAnimatedGradientOnHover(elembent) {
-    element.addEventListener('mouseover', () => {
-        document.body.classList.add('animated-gradient');
-    });
+// function addAnimatedGradientOnHover(elembent) {
+//     element.addEventListener('mouseover', () => {
+//         document.body.classList.add('animated-gradient');
+//     });
 
-    element.addEventListener('mouseout', () => {
-        document.body.classList.remove('animated-gradient');
-    });
-}
+//     element.addEventListener('mouseout', () => {
+//         document.body.classList.remove('animated-gradient');
+//     });
+// }
 
-const container1 = document.getElementById('container1');
-const container2 = document.getElementById('container2');
-const container3 = document.getElementById('container3'); 
+// const container1 = document.getElementById('container1');
+// const container2 = document.getElementById('container2');
+// const container3 = document.getElementById('container3'); 
 
-addAnimatedGradientOnHover(container1);
-addAnimatedGradientOnHover(container2);
-addAnimatedGradientOnHover(container3);
+// addAnimatedGradientOnHover(container1);
+// addAnimatedGradientOnHover(container2);
+// addAnimatedGradientOnHover(container3);
